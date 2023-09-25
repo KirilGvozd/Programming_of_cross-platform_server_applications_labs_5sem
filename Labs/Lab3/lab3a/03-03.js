485583b536d9d9d4b3c39b676b7feee6d96b4cfe
Kirill <kirillgvozd0@gmail.com>
2023-09-25 16:14:03 +0300

    Finished the 3a lab

---
+++ Labs/Lab3/lab3a/03-03.js
@@ -0,0 +1,27 @@
+http = require('http');
+url = require('url');
+fs = require('fs');
+
+let factorial = (k) => {
+    return k <= 1 ? k: k * factorial(k - 1);
+}
+
+http.createServer((req, res) => {
+    let path = url.parse(req.url).pathname;
+    if (path === '/fact') {
+        let param = url.parse(req.url, true).query.k;
+        if (typeof param !== 'undefined') {
+            let k = parseInt(param, 10);
+            if (Number.isInteger(k)) {
+                res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+                res.end(JSON.stringify({ k: k, factorial: factorial(k)}));
+            }
+        }
+    } else if (path === '/') {
+        let html = fs.readFileSync('03-03.html', 'utf8');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html)
+    }
+}).listen(5000);
+
+console.log("The app is running on http://localhost:5000");
